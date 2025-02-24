30be95dd6578e8b77997c3bcc69b894f3e25a7e7
wispard1 <antonvlasov610@gmail.com>
2025-02-25 00:11:42 +0800

    update brain-even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,35 +1,4 @@
-import { generateRandomNumber, isEven, greetUser } from '../src/cli.js';
-import readlineSync from 'readline-sync';
+import runGame from '../src/index.js';
+import { generateRound, gameDescription } from '../src/games/brain-even.js'
 
-const playGame = () => {
-  const name = greetUser();
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-
-  const rounds = 3;
-  let correctCount = 0;
-
-  for (let i = 0; i < rounds; i += 1) {
-    const number = generateRandomNumber();
-    console.log(`Question: ${number}`);
-
-    const userAnswer = readlineSync.question('Your answer: ');
-    const correctAnswers = isEven(number) ? 'yes' : 'no';
-
-    if (userAnswer === correctAnswers) {
-      console.log('Correct!');
-      correctCount += 1;
-    } else {
-      console.log(
-        `'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswers}'`
-      );
-    }
-  }
-
-  if (correctCount === rounds) {
-    console.log(`Congratulations, ${name}!`);
-  } else {
-    console.log(`Let's try again, ${name}!`);
-  }
-};
-
-playGame();
+runGame(generateRound, gameDescription);
