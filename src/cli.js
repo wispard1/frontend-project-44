e23d265967fc9aeeb263a52bf563ce7624c7bcff
wispard1 <antonvlasov610@gmail.com>
2025-02-25 00:29:59 +0800

    update cli.js

--- src/cli.js
+++ src/cli.js
@@ -7,14 +7,3 @@ export const greetUser = () => {
 
   return name;
 };
-
-export const generateRandomNumber = () => {
-  const min = 1;
-  const max = 100;
-  const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
-  return randomNumber;
-};
-
-export const isEven = (number) => {
-  return number % 2 === 0;
-};
