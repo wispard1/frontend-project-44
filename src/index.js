3da37b53692f59cf5b214219d0a3702d2b7dc8c6
wispard1 <antonvlasov610@gmail.com>
2025-02-25 00:33:05 +0800

    add logic for games

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import readlineSync from 'readline-sync';
+
+const runGame = (generateRound, gameDescription) => {
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name: ');
+  console.log(`Hello, ${name}!`);
+
+  console.log(gameDescription);
+
+  const rounds = 3;
+  let correctCount = 0;
+
+  for (let i = 0; i < rounds; i += 1) {
+    const { question, correctAnswer } = generateRound();
+    console.log(`Question: ${question}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+
+    if (userAnswer === correctAnswer) {
+      console.log('Correct!');
+      correctCount += 1;
+    } else {
+      console.log(
+        `'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'`
+      );
+      console.log(`Let's try again, ${name}!`);
+      return;
+    }
+  }
+  if (correctCount === rounds) {
+    console.log(`Congratulations, ${name}!`);
+  }
+};
+export default runGame;
