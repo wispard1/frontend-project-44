cfdd281e10b908249cf581acc8a1a388b83106dc
wispard1 <antonvlasov610@gmail.com>
2025-03-09 01:45:07 +0800

    update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,13 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {languageOptions: { globals: globals.browser }},
+  { 
+    languageOptions: { globals: globals.browser },
+    rules: {
+      "linebreak-style": ["error", "windows"]
+    }
+  },
   pluginJs.configs.recommended,
 ];
