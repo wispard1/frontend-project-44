ee18b83b21ddb41f8d2e2535c5648a082bcad94c
wispard1 <antonvlasov610@gmail.com>
2025-03-14 18:13:39 +0800

    Fix ESLint errors

--- eslint.config.js
+++ eslint.config.js
@@ -7,7 +7,6 @@ export default [
     languageOptions: { globals: globals.browser },
     rules: {
       'linebreak-style': ['error', 'windows'],
-      'operator-linebreak': 'off',
     },
   },
   pluginJs.configs.recommended,
