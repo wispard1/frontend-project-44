dfa9facf4c6da7ef02bd5a7553b774ea96ab110c
wispard1 <antonvlasov610@gmail.com>
2025-03-14 18:25:24 +0800

    Fix ESLint errors

--- eslint.config.js
+++ eslint.config.js
@@ -5,10 +5,17 @@ import pluginJs from '@eslint/js';
 export default [
   {
     languageOptions: { globals: globals.browser },
+    env: {
+      browser: true,
+      es2021: true,
+    },
+    extends: [
+      'eslint:recommended',
+      'plugin:prettier/recommended', // Добавляем поддержку Prettier
+    ],
     rules: {
       'linebreak-style': ['error', 'windows'],
-      quotes: ['error', 'single'],
-      'object-curly-newline': ['error', { consistent: true }],
+      'prettier/prettier': 'error',
     },
   },
   pluginJs.configs.recommended,
