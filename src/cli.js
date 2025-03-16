ae7cfb1c4cf94a4fc33b4f939135f9c49073759d
wispard1 <antonvlasov610@gmail.com>
2025-03-16 16:36:56 +0800

    Fix ESLint errors

--- src/cli.js
+++ src/cli.js
@@ -6,18 +6,22 @@ const greetUser = () => {
   console.log(`Hello, ${name}!`);
   return name;
 };
+
 const generateRandomNumber = (min = 1, max = 100) => {
   const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
   return randomNumber;
 };
+
 const getRandomOperation = () => {
   const operations = ['+', '-', '*'];
   return operations[Math.floor(Math.random() * operations.length)];
 };
+
 const isEven = (number) => number % 2 === 0;
+
 export { 
   greetUser, 
   generateRandomNumber, 
   getRandomOperation, 
-  isEven 
+  isEven, 
 };
